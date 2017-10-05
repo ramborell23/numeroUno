2151714002e72d9757b63f41c9be511aa413bffe
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-10-05 10:16:18 -0400

    initialize

---
+++ age.js
@@ -0,0 +1,20 @@
+// var birthYear = 1990
+// var futureYear = 2022
+// var futureAge = futureYear- birthYear
+// var futureAge2  = futureYear- birthYear-1
+// console.log("I will be either "+futureAge +" or " +futureAge2+" in "+ futureYear,)
+
+var age = 10
+var futureAge = 20
+var meals = 2
+var total = (futureAge - age) * 365 * meals
+console.log( 'You will need ',total ,'to last you until the age of', futureAge)
+
+
+var radius = 3
+
+
+
+
+
+˚
---
+++ variables.js
@@ -0,0 +1,33 @@
+
+// Age
+var birthYear = 1990;
+var future = 2020;
+//console.log(`I will be either ${future - birthYear-1} or ${future - birthYear} in ${future}`)
+
+
+
+//Snacks
+
+var currentAge = 27;
+var maxAge = 50;
+var snacks = 5;
+//console.log(`I will need ${(365*snacks)*(maxAge - currentAge)} to last you until the age of ${maxAge}.`)
+
+
+
+//Circle
+
+var radius = 3
+//console.log(`The circumference is ${2*Math.PI*radius}`)
+//console.log(`The area is ${Math.PI*(Math.pow(radius,2))}`)
+
+
+
+// Temperature Converter
+
+var celsius = 30
+console.log(`${celsius}˚ in Celsius is ${celsius *(9/5)+32}˚ Fahrenheit`)
+
+var fahrenheit = 40
+console.log(`${fahrenheit}˚ in Fahrenheit is ${(fahrenheit - 32)*(5/9)}˚ Celsius`)
+
